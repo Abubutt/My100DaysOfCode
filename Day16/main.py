f0dac8bf753c7e4fa14bae1995d6f3f6a3e69a77
Abubutt <abubutt14@gmail.com>
2021-09-21 19:08:59 -0400

    updated day

--- Day16/main.py
+++ Day16/main.py
@@ -8,21 +8,15 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 while is_machine_on:
-    item = input("What would you like? (espresso/latte/cappuccino): ")
-    if item == "espresso":
-        item = menu.menu[1]
-        if coffee_maker.is_resource_sufficient(item) and money_machine.make_payment(item.cost):
-            coffee_maker.make_coffee(item)
-    elif item == "latte":
-        item = menu.menu[0]
-        if coffee_maker.is_resource_sufficient(item) and money_machine.make_payment(item.cost):
-            coffee_maker.make_coffee(item)
-    elif item == "cappuccino":
-        item = menu.menu[2]
-        if coffee_maker.is_resource_sufficient(item) and money_machine.make_payment(item.cost):
-            coffee_maker.make_coffee(item)
+    options = menu.get_items()
+    item = input(f"What would you like? ({options}): ")
+
+    if item == "off":
+        is_machine_on = False
     elif item == "report":
         coffee_maker.report()
         money_machine.report()
     else:
-        is_machine_on = False
+        drink = menu.find_drink(item)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
